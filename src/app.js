622d6f03f5ed402169688f2ae70d952fd2adb419
Umesh <ums.rbc07@gmail.com>
2026-01-14 21:32:56 +0545

    Impement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Hello from acquistions")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from acquistions');
+});
 
 export default app;
